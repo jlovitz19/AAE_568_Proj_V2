8790924c4888ad0967760a9aae29ffb83358e78c
Jacob <ajacobwolf@gmail.com>
2025-04-22 15:59:00 -0400

    run_sim is written in terms of param now :|

--- run_sim.m
+++ run_sim.m
@@ -1,13 +1,19 @@
 clc; clear; close all;
 
-% define orbit
-R_e = 6.378e6;
-a = R_e + 4600; % r
-ecc = 0; % eccentricity
-M = 0; % mean anomaly
+% define physical parameters
+param.G = 6.6743e-11; % Gravitational constant (m/s^)/(kg/m^2)
+param.Me = 5.9722e24;  % Earth mass (kg)
+param.Re = 6.37836e6; % Radius of the Earth (can be 1 for normalized sphere)
+param.m = 61.6;
+param.mu = param.G*(param.Me+param.m);
+
+% orbit parameters
+param.M = 0;
+param.a = 460e3+param.Re; % semi major axis (m)
+param.e = 0;
 
 % get translational ICs in polar for translation
-trans_IC = kep2polar([a;ecc;M]);
+trans_IC = kep2polar([param.a;param.e;param.M], param);
 
 % extract the ones we care about
 r0 = trans_IC(1); dr0 = trans_IC(3); theta0 = trans_IC(2); dtheta0 = trans_IC(4);
