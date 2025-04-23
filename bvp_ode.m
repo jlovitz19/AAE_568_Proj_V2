8b441253162e68d3cee4b6995d50c6f16ee4c0aa
MyNameDavis <102628967+MyNameDavis@users.noreply.github.com>
2025-04-22 21:03:53 -0400

    Corrected lambda dots in ode function

--- bvp_ode.m
+++ bvp_ode.m
@@ -59,6 +59,16 @@ function y_dot = bvp_ode(t, y, p)
     x_dot = [x_dot1; x_dot2; x_dot3; x_dot4; x_dot5; x_dot6; x_dot7];
 
     % find lambda_dot
+
+    lambda_dot1 = lambda2*(2*mu/x1^3 - x4^2) - lambda4*2*x2*x4/x1^2;
+    lambda_dot2 = 2*lambda4*x4/x1 - lambda1;
+    lambda_dot3 = 0;
+    lambda_dot4 = 2*lambda2*x1*x4 - lambda3 + lambda4*2*x2/x1 - lambda7;
+    lambda_dot5 = 0;
+    lambda_dot6 = -lambda5 - lambda7;
+    lambda_dot7 = -p.a^2;
+
+    %{
     lambda_dot1 = -x2*x4^2 - 2*lambda2*mu/(x1^3) + lambda4*2*x2*x4/(x1^2);
     lambda_dot2 = lambda1 - 2*lambda4*x4/x1;
     lambda_dot3 = 0;
@@ -66,8 +76,9 @@ function y_dot = bvp_ode(t, y, p)
     lambda_dot5 = 0;
     lambda_dot6 = lambda5 + lambda7;
     lambda_dot7 = 2*w1*x7;
+    %}
 
-    lambda_dot = -[lambda_dot1; lambda_dot2; lambda_dot3; lambda_dot4;...
+    lambda_dot = [lambda_dot1; lambda_dot2; lambda_dot3; lambda_dot4;...
         lambda_dot5; lambda_dot6; lambda_dot7];
 
     % combine state and costate derivatives
